156f002f93ed0fa2974a1e5a9674a978c988de76
Flo <teorta3@gmail.com>
2026-05-13 13:25:13 +0200

    la normalement il devrai y avoir un cube rouge, ensuite je m'occuperai des déplacements

--- code/main.c
+++ code/main.c
@@ -1,16 +1,16 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-// Là déja je vais m'occuper de la fenetre parce que j'ai vraiment envie qu'il en ai une,quitte a faire les fonctionnalittée plus tars, en plus j'en avais déja fait une il y a longtemps donc je me rappelle des commandes et tout ce qui va avec
-
 int main(int argc, char* argv[])
 {
+    // Initialisation SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         printf("Erreur SDL : %s\n", SDL_GetError());
         return 1;
     }
 
+    // Création fenêtre
     SDL_Window* window = SDL_CreateWindow(
         "Cluelau",
         SDL_WINDOWPOS_CENTERED,
@@ -27,8 +27,55 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    SDL_Delay(3000);
+    // Renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+    if (!renderer)
+    {
+        printf("Erreur renderer : %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Position joueur
+    int playerX = 100;
+    int playerY = 100;
+
+    // Boucle de jeu
+    int running = 1;
+    SDL_Event event;
+
+    while (running)
+    {
+        // Gestion événements
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = 0;
+            }
+        }
+
+        // Fond noir
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Joueur (carré rouge)
+        SDL_Rect player = {playerX, playerY, 50, 50};
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &player);
+
+        // Affichage
+        SDL_RenderPresent(renderer);
+
+        // petite pause CPU
+        SDL_Delay(16);
+    }
+
+    // Nettoyage
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
